fe7c6d2c1c0a9e0da1922a2c39ffb3631c796a7b
luizmcf <146488704+luizmcf@users.noreply.github.com>
2023-11-23 20:18:20 -0300

    Update wsgi.py

--- crashblog/wsgi.py
+++ crashblog/wsgi.py
@@ -15,3 +15,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crashblog.settings')
 
 application = get_wsgi_application()
+
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
